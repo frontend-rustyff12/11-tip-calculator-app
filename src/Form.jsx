79d476865791e2621eceae91540c0f8ee37e35f4
Russell <75209707+rustyff12@users.noreply.github.com>
2025-08-22 11:59:50 +0200

    moved custom classes to own file. Start of state logic

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,8 +1,31 @@
+import { useState } from "react";
+import { btnClass, labelClass, inputClass } from "./customClasses";
 export default function Form() {
-  const btnClass =
-    "bg-cust-Green-900 py-6 px-8 rounded-xl text-cust-White font-bold text-5xl hover:bg-cust-Green-400 cursor-pointer";
+  const [total, setTotal] = useState("");
+  const [headCount, setHeadCount] = useState("");
+  const [tipPercent, setTipPercent] = useState("");
+  const [tipPerPerson, setTipPerPerson] = useState("$0.00");
+  const [totalPerPerson, setTotalPerPerson] = useState("$0.00");
+  const [validState, setValidState] = useState(true);
 
-  const labelClass = "text-3xl text-cust-Grey-500 font-bold";
+  function handleTotal(e) {
+    setTotal(e.target.value);
+  }
+
+  function handleHeadCount(e) {
+    setHeadCount(e.target.value);
+  }
+
+  function handleClick(e) {
+    e.preventDefault();
+    if (headCount === "") {
+      setValidState(false);
+      return;
+    }
+    setValidState(true);
+    const amount = e.target.value;
+    setTipPercent(amount);
+  }
 
   return (
     <form className="bg-cust-White h-full rounded-t-4xl p-16 grid grid-cols-1 grid-rows-auto gap-8">
@@ -12,8 +35,10 @@ export default function Form() {
         </label>
         <input
           type="text"
-          className="bg-[url('/icon-dollar.svg')] bg-no-repeat bg-[0.5rem_center] bg-[length:1.5rem] bg-gray-50 text-right pl-8 p-6 mt-8 text-4xl"
+          className={`bg-[url('/icon-dollar.svg')] ${inputClass}`}
           name="bill"
+          value={total}
+          onChange={handleTotal}
           placeholder="0"
         />
       </div>
@@ -22,11 +47,21 @@ export default function Form() {
           Select Tip %
         </label>
         <div className="grid grid-cols-2 grid-rows-3 gap-6 mt-8">
-          <button className={btnClass}>5%</button>
-          <button className={btnClass}>10%</button>
-          <button className={btnClass}>15%</button>
-          <button className={btnClass}>25%</button>
-          <button className={btnClass}>50%</button>
+          <button onClick={handleClick} className={btnClass} value="5">
+            5%
+          </button>
+          <button onClick={handleClick} className={btnClass} value="10">
+            10%
+          </button>
+          <button onClick={handleClick} className={btnClass} value="15">
+            15%
+          </button>
+          <button onClick={handleClick} className={btnClass} value="25">
+            25%
+          </button>
+          <button onClick={handleClick} className={btnClass} value="50">
+            50%
+          </button>
           <label htmlFor="custom" className="hidden"></label>
           <input
             className="placeholder:text-center text-right p-2 bg-gray-50 rounded text-5xl font-bold"
@@ -37,13 +72,26 @@ export default function Form() {
         </div>
       </div>
       <div className="flex flex-col">
-        <label className={labelClass} htmlFor="head-count">
-          Number of People
-        </label>
+        <div className="flex justify-between items-center">
+          <label className={labelClass} htmlFor="head-count">
+            Number of People
+          </label>
+          <p
+            className={`text-2xl text-red-400 font-bold ${
+              validState ? "hidden" : ""
+            }`}
+          >
+            Can't be zero
+          </p>
+        </div>
         <input
           type="text"
-          className="bg-[url('/icon-person.svg')] bg-no-repeat bg-[0.5rem_center] bg-[length:1.5rem] bg-gray-50 text-right pl-8 p-6 mt-8 text-4xl"
-          name="Head-count"
+          className={`bg-[url('/icon-person.svg')] ${inputClass} ${
+            validState ? "" : "border border-red-500 rounded p-2"
+          }`}
+          name="head-count"
+          value={headCount}
+          onChange={handleHeadCount}
           placeholder="0"
         />
       </div>
@@ -56,7 +104,9 @@ export default function Form() {
                 / person
               </p>
             </div>
-            <p className="text-cust-Green-400 font-bold text-5xl">$4.27</p>
+            <p className="text-cust-Green-400 font-bold text-5xl">
+              {tipPerPerson}
+            </p>
           </div>
 
           <div className="flex justify-between">
@@ -66,7 +116,9 @@ export default function Form() {
                 / person
               </p>
             </div>
-            <p className="text-cust-Green-400 font-bold text-5xl">$32.79</p>
+            <p className="text-cust-Green-400 font-bold text-5xl">
+              {totalPerPerson}
+            </p>
           </div>
         </div>
         <button className="bg-cust-Green-400 p-6 rounded-xl text-cust-Green-900 font-bold text-4xl hover:bg-cust-Green-400 cursor-pointer uppercase">
---
+++ src/customClasses.js
@@ -0,0 +1,7 @@
+export const btnClass =
+  "bg-cust-Green-900 py-6 px-8 rounded-xl text-cust-White font-bold text-5xl hover:bg-cust-Green-400 cursor-pointer";
+
+export const labelClass = "text-3xl text-cust-Grey-500 font-bold";
+
+export const inputClass =
+  "bg-no-repeat bg-[0.5rem_center] bg-[length:1.5rem] bg-gray-50 text-right pl-8 p-6 mt-8 text-5xl text-cust-Green-900 font-bold  focus:outline rounded-2xl focus:outline-4 focus:outline-cust-Green-900";
